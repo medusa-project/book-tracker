d0909a6f7808de6f0e1c2115885dc42b5ceb8cd8
Alex Dolski <alexd@n-ary.net>
2019-11-13 15:59:10 -0600

    check() tries to touch the database

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -4,6 +4,7 @@ class HealthController < ApplicationController
   # Responds to GET /health
   #
   def check
+    Book.all.limit(1).pluck(:id) # exercise the database
     render plain: 'OK'
   end
 
