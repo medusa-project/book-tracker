ee92c8ea4e602d585730110b26085e24d2aafe71
Alex Dolski <alexd@n-ary.net>
2019-11-27 14:39:20 -0600

    Don't exercise the database in demo

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -4,7 +4,10 @@ class HealthController < ApplicationController
   # Responds to GET /health
   #
   def check
-    Book.all.limit(1).pluck(:id) # exercise the database
+    # exercise the database, except in demo, where the database is running in
+    # Aurora and saves us money while it's idle.
+    Book.all.limit(1).pluck(:id) unless Rails.env.demo?
+
     render plain: 'OK'
   end
 
