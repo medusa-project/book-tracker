d50b5e3852627646ce550988abaa5b65515a17df
Alex Dolski <alexd@n-ary.net>
2022-02-23 14:50:44 -0600

    Remove database check

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -4,10 +4,6 @@ class HealthController < ApplicationController
   # Responds to GET /health
   #
   def check
-    # exercise the database, except in demo, where the database is running in
-    # Aurora and saves us money while it's idle.
-    Book.all.limit(1).pluck(:id) unless Rails.env.demo?
-
     render plain: 'OK'
   end
 
