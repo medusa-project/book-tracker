772ba9676f7b4b06d4404538feff49fccc6b5c76
Gauri Joshi <gauri.joshi@planetargon.com>
2023-08-17 13:46:05 -0700

    Test: Model test for Status::to_s returns correct status value

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Status::to_s returns 'Submitted' for SUBMITTED status" do 
+    assert_equal 'Submitted', Task::Status.to_s(Task::Status::SUBMITTED)
+  end    
+
+  test "Status::to_s returns 'Running' for RUNNING status" do 
+    assert_equal 'Running', Task::Status.to_s(Task::Status::RUNNING)
+  end
+
+  test "Status::to_s returns 'Paused' for PAUSED status" do 
+    assert_equal 'Paused', Task::Status.to_s(Task::Status::PAUSED)
+  end
+
+  test "Status::to_s returns 'Succeeded' for SUCCEEDED status" do
+    assert_equal 'Succeeded', Task::Status.to_s(Task::Status::SUCCEEDED)
+  end
+
+  test "Status::to_s returns 'Failed' for FAILED status" do 
+    assert_equal 'Failed', Task::Status.to_s(Task::Status::FAILED)
+  end
 end
+
+
