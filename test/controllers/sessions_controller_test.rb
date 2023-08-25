59604cb8f9a0c2b225e13866ee6d82ec8b89cfc5
Gauri Joshi <gauri.joshi@planetargon.com>
2023-08-25 14:13:52 -0700

    Test: mock session with omniauth to show successful redirect upon creating session

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,29 +1,28 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # setup do 
-  #   OmniAuth.config.test_mode = true 
-  #   auth_hash = { 'provider' => 'google',
-  #                 'uid' => '34',
-  #                 'info' => {
-  #                     'name' => 'admin',
-  #                     'email' => '123@admin.com',
-  #                   }
-  #               }
-  #   OmniAuth.config.add_mock(:google, auth_hash)
-  #   user.username == 'admin'
-  # end
-  
-  test "should create session" do 
-    skip()
-    post sessions_url 
+
+  setup do 
+    OmniAuth.config.test_mode = true 
+    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+      provider:'google',
+      uid:'admin@email.com',
+      info: {
+        name:'admin',
+        email: '123@admin.com'
+        }
+      })
   end
+    
+  test "should create session" do 
+
+    post '/auth/google/callback'
+    assert_redirected_to root_url 
+    follow_redirect!
+
+    assert_response :redirect 
+  end
+  
 end
 
-  # test "should destroy session" do
-  #   assert_difference("Session.count", -1) do
-  #     delete session_url(@session)
-  #   end
 
-  #   assert_redirected_to sessions_url
-  # end
