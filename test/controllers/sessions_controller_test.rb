2e1088505fe97bac0d8ecf624a5b0e55d9ab25ac
Gauri Joshi <gauri.joshi@planetargon.com>
2023-08-28 16:12:27 -0700

    Test: adds method to simulate user login and tests a failed login redirect to root path

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,28 +1,36 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+# do I need this setup if I have the auth hash setup in the test_helper?
+  # setup do 
+  #   OmniAuth.config.test_mode = true 
+  #   OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+  #     provider:'google',
+  #     uid:'admin@email.com',
+  #     info: {
+  #       name:'admin',
+  #       email: '123@admin.com'
+  #       }
+  #     })
+  # end
 
-  setup do 
-    OmniAuth.config.test_mode = true 
-    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-      provider:'google',
-      uid:'admin@email.com',
-      info: {
-        name:'admin',
-        email: '123@admin.com'
-        }
-      })
+  test "should redirect to books_path if sign in succeeds" do 
+    skip()
+    # assert_redirected_to books_path
+    # follow_redirect! 
+    # assert_response :redirect
   end
     
-  test "should create session" do 
+  test "should redirect to root path if sign in fails" do 
 
-    post '/auth/google/callback'
-    assert_redirected_to root_url 
+    post '/auth/:provider/callback'
+  
+    assert_redirected_to root_url
     follow_redirect!
 
     assert_response :redirect 
+    
   end
-  
 end
 
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,23 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  ##
+# @param user [User]
+#
+  def log_in_as(user)
+    post "/auth/shibboleth/callback", env: {
+      "omniauth.auth": {
+        provider:          "shibboleth",
+        "Shib-Session-ID": SecureRandom.hex,
+        uid:               user.email,
+        info: {
+          email: user.email
+        },
+        extra: {
+          raw_info: {
+          }
+        }
+      }
+    }
+  end
 end
