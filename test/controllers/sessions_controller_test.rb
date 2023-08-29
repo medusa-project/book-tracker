d186189bc123a393862f48fed98e35f0a62e29fe
Gauri Joshi <gauri.joshi@planetargon.com>
2023-08-29 10:38:43 -0700

    Test: if successful login, user is taken to books_path otherwise redirected to root; user logging out also redirect to root

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,35 +1,34 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-# do I need this setup if I have the auth hash setup in the test_helper?
-  # setup do 
-  #   OmniAuth.config.test_mode = true 
-  #   OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-  #     provider:'google',
-  #     uid:'admin@email.com',
-  #     info: {
-  #       name:'admin',
-  #       email: '123@admin.com'
-  #       }
-  #     })
-  # end
 
   test "should redirect to books_path if sign in succeeds" do 
-    skip()
-    # assert_redirected_to books_path
-    # follow_redirect! 
-    # assert_response :redirect
+    post '/auth/:provider/callback'
+
+    log_in
+    
+    assert_redirected_to books_path
+    follow_redirect! 
+
+    assert_response :success
   end
     
   test "should redirect to root path if sign in fails" do 
-
     post '/auth/:provider/callback'
   
     assert_redirected_to root_url
     follow_redirect!
 
     assert_response :redirect 
-    
+  end
+
+  test "should logout" do 
+    delete '/signout'
+
+    assert_redirected_to root_url 
+    follow_redirect!
+
+    assert_response :redirect
   end
 end
 
