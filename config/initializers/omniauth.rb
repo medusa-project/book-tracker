8b2ebc16b8b5f5024a947c5a98108ca4dda36164
Alex Dolski <alexd@n-ary.net>
2023-10-26 15:42:15 -0500

    Require saml configuration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -14,15 +14,15 @@ Rails.application.config.middleware.use OmniAuth::Builder do
                 File.read(File.join(certs_dir, "sp-cert-#{Rails.env}.pem"))
   config    = ::Configuration.instance
   provider :saml,
-           sp_entity_id:            config.saml&[:sp_entity_id],
-           idp_sso_service_url:     config.saml&[:idp_sso_service_url],
-           idp_sso_service_binding: config.saml&[:idp_sso_service_binding],
+           sp_entity_id:            config.saml[:sp_entity_id],
+           idp_sso_service_url:     config.saml[:idp_sso_service_url],
+           idp_sso_service_binding: config.saml[:idp_sso_service_binding],
            idp_sso_service_url_runtime_params: {
              original_request_param: :mapped_idp_param
            },
            idp_cert:               idp_cert,
            certificate:            sp_cert,
-           private_key:            config.saml&[:sp_private_key],
+           private_key:            config.saml[:sp_private_key],
            name_identifier_format: "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
 end
 
