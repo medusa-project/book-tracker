bf49d5aab5f9a0feae60496950bee6b9a4f52e0a
Gauri Joshi <gauri@illinois.edu>
2023-11-15 13:51:11 -0800

    Specify provider auth routes for developer and google_oauth2 callbacks (pointing to two different actions in sessions controller) (need to refactor later)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post],
-        as: 'auth' # used by omniauth
-  match "/auth/failure", to: "sessions#auth_failed", as: :auth_failed, via: [:get, :post]
+  match '/auth/google_oauth2/callback', to: 'sessions#create', via: [:get, :post], as: 'auth'
+  match '/auth/developer/callback', to: 'sessions#developer', via: [:get, :post]
+        # as: 'auth' # used by omniauth
+  match '/auth/failure', to: 'sessions#auth_failed', as: :auth_failed, via: [:get, :post]
   match '/signout', to: 'sessions#destroy', via: :delete
 
   match 'books', to: 'books#index', as: :books, via: [:get, :post]
