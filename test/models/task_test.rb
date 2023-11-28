82a47b04cc167cac5b7e5194b93bc35060ef5c3e
Gauri Joshi <gauri@illinois.edu>
2023-11-28 13:48:02 -0800

    Test: creating new Task object with default attrs, and testing how import and hathitrust check triggers work

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -20,6 +20,48 @@ class TaskTest < ActiveSupport::TestCase
   test "Status::to_s returns 'Failed' for FAILED status" do 
     assert_equal 'Failed', Task::Status.to_s(Task::Status::FAILED)
   end
+
+  test "Task.new creates new task object defaulting to status of 'RUNNING' (1) and service of nil" do 
+    task = Task.new 
+
+    assert_equal 1, task.status
+    assert_nil task.service 
+  end
+
+  test "Status can be equal to SUCCEEDED (3)" do 
+    task = Task.new 
+    task.status=(3)
+
+    assert_equal 3, task.status 
+  end
+
+  test "Service can be set to equal LOCAL_STORAGE" do 
+    task = Task.new 
+    task.service=(Service::LOCAL_STORAGE)
+
+    assert_equal 3, task.service 
+  end
+
+  test ".import_task_completed? will return true under correct conditions" do 
+    task = Task.new 
+    task.status=(4)
+    task.service=(Service::LOCAL_STORAGE) 
+
+    assert_equal true, task.import_task_completed?
+  end
+
+  test ".trigger_hathitrust_check will start the hathitrust check process" do 
+    task = Task.new
+    task.status=(4)
+    task.service=(Service::LOCAL_STORAGE)
+    task.import_task_completed?
+
+    assert_not_nil task.trigger_hathitrust_check
+  end
+
 end
+
+
+
 
 
