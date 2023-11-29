ca4858aeb69a25a21086ce82072ee7e5432fa171
Gauri Joshi <gauri@illinois.edu>
2023-11-29 09:46:52 -0800

    Remove tests for check_hathitrust_path

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -32,21 +32,6 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     assert_equal "/check-google", request.path 
   end
 
-  test "should conduct a Hathitrust check" do
-    
-    post check_hathitrust_url
-
-    assert_equal 200, response.status
-  end
-  
-  test "should redirect back to '/check-hathitrust' after conducting Hathitrust check" do 
-    
-    post check_hathitrust_url
-    
-    assert_equal "/check-hathitrust", request.path 
-    
-  end
-
   test "should conduct a Internet Archive check" do 
 
     post check_internet_archive_url 
