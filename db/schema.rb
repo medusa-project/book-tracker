d234507542581d8f2bc9518df1b490b7a1365d41
Gauri Joshi <gauri@illinois.edu>
2024-01-31 09:22:10 -0800

    Rollback migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_223528) do
+ActiveRecord::Schema[7.1].define(version: 2022_09_16_162423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_24_223528) do
     t.text "raw_marcxml"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
-    t.string "cover_filename"
     t.index ["author"], name: "index_books_on_author"
     t.index ["bib_id"], name: "index_books_on_bib_id"
     t.index ["date"], name: "index_books_on_date"
